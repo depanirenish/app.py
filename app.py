19bd45644d105570d1cf77bf91ced5b3d068b9eb
renishdepani <depanirenish@gmail.com>
2024-07-16 14:27:36 +0530

    chn-14

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     name = os.environ["NAME"]
-    return '<h1>Hello, World---change! '+ name  +'</h1>'
+    return '<h1>Hello, World 1. '+ name  +'</h1>'
 
 if __name__ == '__main__':
     # Specify the custom port number here
